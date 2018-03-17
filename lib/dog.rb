80248f2b1eec060ca753d03511f3137014040bba
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-17 21:49:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Dog
+  def initialize(dog_name, dog_bark)
+    @name = dog_name
+    @bark = dog_bark
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+
+  def bark=(dog_bark)
+    @bark = dog_bark
+  end
+
+  def bark
+    puts "woof!"
+  end            
+end
